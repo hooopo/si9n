9033aaad2b2c53f95c1fb97b0d22fafc2846510b
Hooopo <Hoooopo@gmail.com>
2010-12-23 01:52:19 +0800

    remove vender/cache

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,6 @@
 class HomeController < ApplicationController
   #before_filter :douban_auth_required
   def index
-
-#    p session[:douban]
-#    p "-" * 10
-#    require 'json'
-    p  JSON.parse(cookies.signed[:douban]).class if cookies.signed[:douban]
-    if douban
-     p 1
-      #log ger.debug("ooo")
-      #douban.miniblog(session)
-    end
   end
 
 end
