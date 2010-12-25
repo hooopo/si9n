2fb1cfae270b93141752ef1520e2ddc5ebb2442d
Hooopo <Hoooopo@gmail.com>
2010-12-26 03:23:50 +0800

    change dialog width

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -1,26 +1,6 @@
 $(function(){
-
-
-    // Dialog
     $('#dialog').dialog({
         autoOpen: false,
-        width: 600
+        width: 540
     });
-
-// Dialog Link
-//    $('.signature-new').click(function(){
-//        $('#dialog').dialog('open');
-//        return false;
-//    });
-
-//    //hover states on the static widgets
-//    $('#dialog_link, ul#icons li').hover(
-//        function() {
-//            $(this).addClass('ui-state-hover');
-//        },
-//        function() {
-//            $(this).removeClass('ui-state-hover');
-//        }
-//        );
-
 });
