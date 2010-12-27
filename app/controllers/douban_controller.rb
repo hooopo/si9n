015e1b27eacca5306a1b05a0cf31719001e04eab
Hooopo <Hoooopo@gmail.com>
2010-12-28 01:37:25 +0800

    store user info ..

--- app/controllers/douban_controller.rb
+++ app/controllers/douban_controller.rb
@@ -17,6 +17,19 @@ class DoubanController < ApplicationController
       douban.authorize
       session[:douban] = douban.dump
       cookies.permanent.signed[:douban] = douban.dump.try(:to_json)
+      resp = douban.get("/people/%40me")
+      if resp.message == "OK"
+        require 'pp'
+        xml = resp.body
+        user_info = ActiveSupport::XmlMini.parse(xml)
+        pp user_info["entry"]
+        user_info = user_info["entry"]
+        User.create :location => user_info["location"]["id"],
+          :uid => user_info["uid"]["__content__"],
+          :content => user_info["content"]["__content__"],
+          :link => user_info["link"].find{|link| link["rel"] == "alternate"}["href"]
+      end
+
     end
     
     redirect_to(session[:back] || '/')
