712159ca24aed049f5ea2a9efa644b2b1c8bfc1a
qichunren <whyruby@gmail.com>
2011-01-23 14:32:10 +0800

    完成了个人中心里的tab样式

--- app/helpers/mine_helper.rb
+++ app/helpers/mine_helper.rb
@@ -1,3 +1,12 @@
 # encoding: utf-8
-module MineHelper
+module MineHelper   
+            
+  # highlight for current user mine tabs item
+  def current_class_for_mine(action)
+    if params[:controller] == "mine" && params[:action] == action
+      "class='selected'"
+    else
+      ""
+    end 
+  end
 end
