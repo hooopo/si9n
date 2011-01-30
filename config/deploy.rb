fef4fa408a537287970b4f4453a4d7f7c9090311
Hooopo <Hoooopo@gmail.com>
2011-01-30 17:42:56 +0800

    use cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,33 +67,3 @@ end
 
 after 'deploy:update_code', 'bundler:symlink_bundled_gems'
 after 'deploy:update_code', 'bundler:install'
-
-
-#default_run_options[:pty] = true  # Must be set for the password prompt from git to work
-#set :repository, "git@github.com:vanpelt/rails-app.git"  # Your clone URL
-#set :scm, "git"
-#set :user, "root1"  # The server's user for deploys
-#set :scm_passphrase, "ocnc123"  # The deploy user's password
-#set :deploy_via, :remote_cache
-#set :application, "si9n"
-#set :repository,  "git://github.com/hooopo/si9n.git"
-#
-##set :scm, :subversion
-### Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-##
-#role :web, "hooopo.com:50022"                          # Your HTTP server, Apache/etc
-#role :app, "hooopo.com:50022"                          # This may be the same as your `Web` server
-#role :db,  "hooopo.com:50022", :primary => true # This is where Rails migrations will run
-###role :db,  "your slave db-server here"
-#
-## If you are using Passenger mod_rails uncomment this:
-## if you're still using the script/reapear helper you will need
-## these http://github.com/rails/irs_process_scripts
-#
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
