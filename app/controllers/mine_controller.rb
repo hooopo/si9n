a2633b93c7002b8a71775870552c3974b55624ee
Hooopo <Hoooopo@gmail.com>
2011-02-13 03:21:07 +0800

    添加分页

--- app/controllers/mine_controller.rb
+++ app/controllers/mine_controller.rb
@@ -4,11 +4,11 @@ class MineController < ApplicationController
   before_filter :douban_auth_required
 
   def signatures
-    @signatures = current_user.signatures
+    @signatures = current_user.signatures.order("id DESC").paginate :per_page => 15, :page => params[:page]
   end
 
   def favorites
-    @signatures = current_user.favorite_signatures
+    @signatures = current_user.favorite_signatures.order("id DESC").paginate :per_page => 15, :page => params[:page]
   end
 
   def settings
---
+++ config/initializers/will_paginate_patch.rb
@@ -0,0 +1,3 @@
+# encoding: utf-8
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = '上一页'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = '下一页'
