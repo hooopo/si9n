e8e25e5538e7aff10c32f45bfb9e45bafa000f54
Hooopo <Hoooopo@gmail.com>
2011-02-19 07:00:54 +0800

    添加will paginate patch

--- config/initializers/will_paginate_patch.rb
+++ config/initializers/will_paginate_patch.rb
@@ -1,3 +1,9 @@
 # encoding: utf-8
 WillPaginate::ViewHelpers.pagination_options[:previous_label] = '上一页'
 WillPaginate::ViewHelpers.pagination_options[:next_label] = '下一页'
+module WillPaginate::ViewHelpers::ActionView
+  def paginated_section(*args, &block)
+    pagination = will_paginate(*args).to_s
+    (pagination + capture(&block) + pagination).try(:html_safe)
+  end
+end
