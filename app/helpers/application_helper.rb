433eff59685b6c2af414903b5829c5978d53ddc9
Hooopo <hoooopo@gmail.com>
2011-04-03 02:26:09 +0800

    添加分享按钮

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -43,5 +43,5 @@ module ApplicationHelper
       redirect_to root_path
     end
   end
-  
+
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -19,4 +19,40 @@ $(function(){
 });
 
 
+function postToQqWeibo(url, title){
+    var _t = encodeURI(title);
+    var _url = encodeURIComponent(url);
+    var _appkey = encodeURI('03acaf417aa94e22ae4956e9c980514a');//你从腾讯获得的appkey
+    var _pic = encodeURI('');//（例如：var _pic='图片url1|图片url2|图片url3....）
+    var _site = 'http://www.hooopo.com';//你的网站地址
+    var _u = 'http://v.t.qq.com/share/share.php?url='+_url+'&appkey='+_appkey+'&site='+_site+'&pic='+_pic+'&title='+_t;
+    openWindow(_u);
+}
 
+function postToSinaWeibo(url, title){
+    var _t = encodeURI(title);
+    var _url = encodeURIComponent(url);
+    var _appkey = encodeURI('403062089');
+    var _pic = encodeURI('');
+    var _relatedUid = "";
+    var _u = "http://service.t.sina.com.cn/share/share.php?url=" + _url +  "&appkey=" + _appkey + "&title=" + _t + "&pic=" + _pic + "&ralateUid=" + _relatedUid;
+    openWindow(_u);
+}
+
+function shareToDouban(url, title){
+    var _t   = encodeURI(title);
+    var _url = encodeURIComponent(url);
+    var _u   = "http://www.douban.com/recommend/?url=" + _url + "&title=" + _t;
+    openWindow(_u);
+}
+
+function shareToRenren(url, title){
+    var _t   = encodeURI(title);
+    var _url = encodeURIComponent(url);
+    var _u   = "http://share.renren.com/share/buttonshare.do?link=" + _url + "&title=" + _t;
+    openWindow(_u);
+}
+
+function openWindow(url){
+    window.open(url, '语录网', 'width=700, height=680, top=0, left=0, toolbar=no, menubar=no, scrollbars=no, location=yes, resizable=no, status=no');
+}
